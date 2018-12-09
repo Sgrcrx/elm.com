4fb6e733d06de2d38ea6465aefd35261e5f5bca7
Sgrcrx <hengmeilengshu1314@qq.com>
2018-12-09 18:35:18 +0800

    elm

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -2,8 +2,15 @@
 	<div class="top-bar">
 		<div class="nav">
 			<div class="address">
-				<i class="fa fa-address-book"></i>
+				<i class="fa fa-map-marker"></i>
 				<h1>请输入地址...</h1>
+				<i class="fa fa-caret-down"></i>
+			</div>
+			<div class="search-item">
+				<a href="#">
+					<i class="fa fa-search"></i>
+					<span class="names">搜索饿了么商家、商品名称</span>
+				</a>
 			</div>
 		</div>
 	</div>
@@ -16,5 +23,48 @@
 </script>
 
 <style scoped="scoped">
-
+	.top-bar {
+		width: 100%;
+	    height: 110px;
+	    background-color: #00a4ff;
+	}
+	.address{
+		color: #fff;
+		box-sizing: border-box;
+		padding: 0px 4px;
+	}
+	.address i{
+		margin-left: 10px;
+		font-size: 20px;
+	}
+	.address h1{
+		display: inline-block;
+		font-size: 18px;
+		margin-left: 10px;
+	}
+	
+	.search-item{
+		width: 100%;
+		height: 100%;
+		box-sizing: border-box;
+		padding: 10px;
+		text-align: center;
+		margin-top: 12px;
+		/*background-color: red;*/
+	}
+	.search-item a{
+		width: 100%;
+		height: 100%;
+		background-color: #fff;
+		box-sizing: border-box;
+		padding: 10px 80px;
+		text-decoration: none;
+		font-size: 16px;
+		color: #999;
+	}
+	.search-item a i{
+		margin-right: 10px;
+	}
+	.search-item a .names{}
+	
 </style>
